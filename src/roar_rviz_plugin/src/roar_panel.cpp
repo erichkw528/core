f5715360f15278b15e9392b1c8d39b67353e7e6b
Michael Wu <wuxiaohua1011@gmail.com>
2023-08-02 00:41:27 -0700

    visualization updated

--- src/roar_rviz_plugin/src/roar_panel.cpp
+++ src/roar_rviz_plugin/src/roar_panel.cpp
@@ -11,11 +11,10 @@ namespace roar_rviz_plugin
     ROARPanel::ROARPanel(QWidget *parent)
         : Panel(parent)
     {
-        this->lifecycle_nodes = new std::vector<std::string>;
-        this->lifecycle_nodes->push_back("/costmap_node_manager");
-        this->lifecycle_nodes->push_back("/global_planner_manager");
-        this->lifecycle_nodes->push_back("/simple_local_planner");
+        node = rclcpp::Node::make_shared("ROARPanel");
 
+        client =
+            node->create_client<roar_msgs::srv::ToggleControlSafetySwitch>("/controller/manager/safety_toggle");
         this->setupUI();
     }
 
@@ -25,26 +24,18 @@ namespace roar_rviz_plugin
 
     void ROARPanel::setupUI()
     {
-        configure_button_ = new QPushButton;
-        activate_button_ = new QPushButton;
-        deactivate_button_ = new QPushButton;
-        cleanup_button_ = new QPushButton;
-
-        configure_button_->setText(QString("Configure"));
-        activate_button_->setText(QString("Activate"));
-        deactivate_button_->setText(QString("Deactivate"));
-        cleanup_button_->setText(QString("Cleanup"));
-
-        connect(configure_button_, &QPushButton::released, this, &ROARPanel::handleConfigureClicked);
-        connect(activate_button_, &QPushButton::released, this, &ROARPanel::handleActivateClicked);
-        connect(deactivate_button_, &QPushButton::released, this, &ROARPanel::handleDeactivateClicked);
-        connect(cleanup_button_, &QPushButton::released, this, &ROARPanel::handleCleanup);
-
         QVBoxLayout *main_layout = new QVBoxLayout;
-        main_layout->addWidget(configure_button_);
-        main_layout->addWidget(activate_button_);
-        main_layout->addWidget(deactivate_button_);
-        main_layout->addWidget(cleanup_button_);
+
+        safetyButton = new QPushButton;
+        safetyButton->setText(QString("Safety Toggle: canSendControl -> OFF"));
+        connect(safetyButton, &QPushButton::released, this, &ROARPanel::handleSafetyButtonTapped);
+        main_layout->addWidget(safetyButton);
+
+        emergencyBrakeButton = new QPushButton;
+        emergencyBrakeButton->setText(QString("[NOT IMPLEMENTED]emergencyBrake: OFF"));
+        connect(emergencyBrakeButton, &QPushButton::released, this, &ROARPanel::handleEmergencyBrakeButtonTapped);
+        main_layout->addWidget(emergencyBrakeButton);
+
         main_layout->setContentsMargins(10, 10, 10, 10);
         setLayout(main_layout);
     }
@@ -65,55 +56,22 @@ namespace roar_rviz_plugin
     }
 
     void
-    ROARPanel::handleConfigureClicked() 
+    ROARPanel::handleSafetyButtonTapped()
     {
-        for (const auto &current_string : *this->lifecycle_nodes)
-        {
-            std::cout << "--- Configuring " << current_string.c_str() << " ---" << std::endl;
-            auto curr_command = "ros2 lifecycle set " + current_string + " configure";
-            system(curr_command.c_str());
-        }
-
-        std::cout << "Attempted to configure " << this->lifecycle_nodes->size() << " nodes" << std::endl;
+        this->canSendControl = !this->canSendControl;
+        auto request = std::make_shared<roar_msgs::srv::ToggleControlSafetySwitch::Request>();
+        request->is_safety_on = this->canSendControl;
+        auto result = client->async_send_request(request);
+        safetyButton->setText(QString("Safety Toggle: canSendControl -> ") + (this->canSendControl ? "YES" : "NO"));
     }
 
     void
-    ROARPanel::handleActivateClicked()
+    ROARPanel::handleEmergencyBrakeButtonTapped()
     {
-        for (const auto &current_string : *this->lifecycle_nodes)
-        {
-            std::cout << "--- Activating " << current_string.c_str() << " ---" << std::endl;
-            auto curr_command = "ros2 lifecycle set " + current_string + " activate";
-            system(curr_command.c_str());
-        }
+        this->emergencyBrakeOn = !this->emergencyBrakeOn;
+        emergencyBrakeButton->setText(QString("[NOT IMPLEMENTED]emergencyBrake:") + (this->emergencyBrakeOn ? "ON" : "OFF"));
 
-        std::cout << "Attempted to activate " << this->lifecycle_nodes->size() << " nodes" << std::endl;
-    }
-
-    void
-    ROARPanel::handleDeactivateClicked()
-    {
-        for (const auto &current_string : *this->lifecycle_nodes)
-        {
-            std::cout << "--- Deactivating " << current_string.c_str() << " ---" << std::endl;
-            auto curr_command = "ros2 lifecycle set " + current_string + " deactivate";
-            system(curr_command.c_str());
-        }
-
-        std::cout << "Attempted to deactivate " << this->lifecycle_nodes->size() << " nodes" << std::endl;
-    }
-
-    void
-    ROARPanel::handleCleanup()
-    {
-        for (const auto &current_string : *this->lifecycle_nodes)
-        {
-            std::cout << "--- Cleaning up " << current_string.c_str() << " ---" << std::endl;
-            auto curr_command = "ros2 lifecycle set " + current_string + " cleanup";
-            system(curr_command.c_str());
-        }
-
-        std::cout << "Attempted to cleanup " << this->lifecycle_nodes->size() << " nodes" << std::endl;
+        std::cout << "NOT IMPLEMNTED" << std::endl;
     }
 } // namespace roar_rviz_plugin
 #include <pluginlib/class_list_macros.hpp> // NOLINT
