e734584677a128661d10a94bbcc74629faff5dec
Michael Wu <wuxiaohua1011@gmail.com>
2023-08-29 08:39:17 -0700

    structure designed, but node not yet running

---
+++ src/planning/behavior_planning/src/default_behavior_planner.cpp
@@ -0,0 +1,38 @@
+#include "behavior_planning/default_behavior_planner.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+namespace roar
+{
+    namespace planning
+    {
+        namespace behavior_planning
+        {
+            namespace impl
+            {
+                DefaultBehaviorPlanner::DefaultBehaviorPlanner(const rclcpp::NodeOptions &options)
+                    : base::BehaviorPlannerBTLifeCycleNode(options)
+                {
+                    RCLCPP_INFO(get_logger(), "Constructing Default Behavior Planner");
+                }
+
+                void DefaultBehaviorPlanner::Initialize()
+                {
+                    RCLCPP_INFO(get_logger(), "Initializing Default Behavior Planner");
+                }
+
+                void DefaultBehaviorPlanner::RunTree()
+                {
+                    RCLCPP_INFO(get_logger(), "Running Default Behavior Planner");
+                }
+
+                void DefaultBehaviorPlanner::PostRunTree()
+                {
+                    RCLCPP_INFO(get_logger(), "Post Running Default Behavior Planner");
+                }
+            } // namespace impl
+        }     // namespace behavior_planning
+    }         // namespace planning
+} // namespace roar
+
+RCLCPP_COMPONENTS_REGISTER_NODE(
+    roar::planning::behavior_planning::impl::DefaultBehaviorPlanner)
