7ad7674be34c554c450386125541cc7a86833449
Michael Wu <wuxiaohua1011@gmail.com>
2023-09-03 18:13:11 -0700

    vehicle_state updated

--- src/misc/vehicle_state_manager/include/vehicle_state_manager/vehicle_state_manager_node.hpp
+++ src/misc/vehicle_state_manager/include/vehicle_state_manager/vehicle_state_manager_node.hpp
@@ -2,7 +2,8 @@
 #define ROAR__VEHICLE_STATE_MANAGER_NODE_HPP_
 
 #include "nav2_util/lifecycle_node.hpp"
-
+#include "roar_msgs/msg/vehicle_state.hpp"
+#include "roar_msgs/msg/vehicle_control.h"
 namespace roar
 {
     class VehicleStateManagerNode : public nav2_util::LifecycleNode
@@ -18,8 +19,22 @@ namespace roar
         nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state) override;
         nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state) override;
 
+        roar_msgs::msg::VehicleState::SharedPtr vehicle_state = nullptr;
+        std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<roar_msgs::msg::VehicleState>> vehicle_state_pub_;
+
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
+        void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+        
+        rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscriber_;
+        void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+
+        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr speedometer_sub_;
+        void speedometer_callback(const std_msgs::msg::Float32::SharedPtr msg);
+
+        rclcpp::Subscription<roar_msgs::msg::VehicleControl>::SharedPtr vehicle_control_sub_;
+        void vehicle_control_callback(const roar_msgs::msg::VehicleControl::SharedPtr msg);
+
     private:
-        void on_update();
         rclcpp::TimerBase::SharedPtr update_timer;
         void update_callback();
     };
--- src/misc/vehicle_state_manager/src/vehicle_state_manager_node.cpp
+++ src/misc/vehicle_state_manager/src/vehicle_state_manager_node.cpp
@@ -5,7 +5,10 @@ using namespace roar;
 
 VehicleStateManagerNode::VehicleStateManagerNode() : nav2_util::LifecycleNode("vehicle_state_manager")
 {
-    RCLCPP_INFO(get_logger(), "vehicle_state_manager_node is now online.");
+    this->declare_parameter("frame_id", "ego_vehicle");
+    this->declare_parameter("loop_rate", 20.0); // 20 hz
+
+    vehicle_state = std::make_shared<roar_msgs::msg::VehicleState>();
 }
 
 VehicleStateManagerNode::~VehicleStateManagerNode()
@@ -15,18 +18,37 @@ VehicleStateManagerNode::~VehicleStateManagerNode()
 nav2_util::CallbackReturn VehicleStateManagerNode::on_configure(const rclcpp_lifecycle::State &state)
 {
     RCLCPP_INFO(get_logger(), "vehicle_state_manager_node is now configuring.");
-    update_timer = create_wall_timer(std::chrono::milliseconds(100), std::bind(&VehicleStateManagerNode::update_callback, this));
+    double loop_rate = this->get_parameter("loop_rate").as_double();
+    int loop_duration_millis = (int)(1000.0 / loop_rate);
+    update_timer = create_wall_timer(std::chrono::milliseconds(loop_duration_millis), std::bind(&VehicleStateManagerNode::update_callback, this));
+    vehicle_state_pub_ = this->create_publisher<roar_msgs::msg::VehicleState>(
+        "/roar/vehicle_state", 10);
+
+    odom_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "/roar/odometry", rclcpp::SystemDefaultsQoS(), std::bind(&VehicleStateManagerNode::odom_callback, this, std::placeholders::_1));
+
+    imu_subscriber_ = this->create_subscription<sensor_msgs::msg::Imu>(
+        "/roar/imu", rclcpp::SystemDefaultsQoS(), std::bind(&VehicleStateManagerNode::imu_callback, this, std::placeholders::_1));
+
+    speedometer_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+        "/roar/speedometer", rclcpp::SystemDefaultsQoS(), std::bind(&VehicleStateManagerNode::speedometer_callback, this, std::placeholders::_1));
+
+    vehicle_control_sub_ = this->create_subscription<roar_msgs::msg::VehicleControl>(
+        "/roar/vehicle_control", rclcpp::SystemDefaultsQoS(), std::bind(&VehicleStateManagerNode::vehicle_control_callback, this, std::placeholders::_1));
+
     return nav2_util::CallbackReturn::SUCCESS;
 }
 
 nav2_util::CallbackReturn VehicleStateManagerNode::on_activate(const rclcpp_lifecycle::State &state)
 {
     RCLCPP_INFO(get_logger(), "vehicle_state_manager_node is now active.");
+    vehicle_state_pub_->on_activate();
     return nav2_util::CallbackReturn::SUCCESS;
 }
 nav2_util::CallbackReturn VehicleStateManagerNode::on_deactivate(const rclcpp_lifecycle::State &state)
 {
     RCLCPP_INFO(get_logger(), "vehicle_state_manager_node is now inactive.");
+    vehicle_state_pub_->on_deactivate();
     return nav2_util::CallbackReturn::SUCCESS;
 }
 
@@ -42,9 +64,34 @@ nav2_util::CallbackReturn VehicleStateManagerNode::on_shutdown(const rclcpp_life
     return nav2_util::CallbackReturn::SUCCESS;
 }
 
+void VehicleStateManagerNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+    vehicle_state->odometry = *msg;
+}
+
+void VehicleStateManagerNode::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+{
+    vehicle_state->imu = *msg;
+}
+
+void VehicleStateManagerNode::speedometer_callback(const std_msgs::msg::Float32::SharedPtr msg)
+{
+    vehicle_state->speedometer = *msg;
+}
+
+void VehicleStateManagerNode::vehicle_control_callback(const roar_msgs::msg::VehicleControl::SharedPtr msg)
+{
+    vehicle_state->vehicle_control = *msg;
+}
+
 void VehicleStateManagerNode::update_callback()
 {
-    RCLCPP_INFO(get_logger(), "vehicle_state_manager_node is now updating.");
+    // update vehicle state
+    vehicle_state->header.stamp = this->now();
+    vehicle_state->header.frame_id = this->get_parameter("frame_id").as_string();
+
+    // publish vehicle state
+    vehicle_state_pub_->publish(*vehicle_state);
 }
 
 int main(int argc, char **argv)
