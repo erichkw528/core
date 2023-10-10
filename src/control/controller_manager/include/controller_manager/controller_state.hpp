47b869165d2d651f4a3a8338d1e4b6d801b6d5a6
Michael Wu <wuxiaohua1011@gmail.com>
2023-10-10 08:08:30 -0700

    lite: trapezoidal_controller

--- src/control/controller_manager/include/controller_manager/controller_state.hpp
+++ src/control/controller_manager/include/controller_manager/controller_state.hpp
@@ -2,6 +2,8 @@
 #define ROAR_CONTROL__PLUGIN__STATE_HPP_
 #include "nav_msgs/msg/path.hpp"
 #include "roar_msgs/msg/behavior_status.hpp"
+#include "roar_msgs/msg/vehicle_state.hpp"
+
 namespace roar
 {
     namespace control
@@ -12,6 +14,7 @@ namespace roar
 
             nav_msgs::msg::Path path_ego_centric;
             roar_msgs::msg::BehaviorStatus::SharedPtr behavior_status;
+            roar_msgs::msg::VehicleState::SharedPtr vehicle_state;
         };
 
         struct ControllerManagerConfig
